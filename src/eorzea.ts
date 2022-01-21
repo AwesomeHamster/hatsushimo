8f144091297d7381dce33d74d5775fe78af7163e
Maiko Tan <maiko.tan.coding@gmail.com>
2022-01-21 14:23:23 +0000

    refactor: format, clean and minor refactor code

--- src/eorzea.ts
+++ src/eorzea.ts
@@ -1,22 +1,14 @@
+import { Context, template } from "koishi";
 
-import { Context, Schema, template } from "koishi";
-
-import {ToEorzeaTime} from "./eorzeaDateTime";
+import { ToEorzeaTime } from "./eorzeaDateTime";
 import eorzeaTemplates from "./template";
 
 export interface EorzeaOptions {
   /**
-   * only reply restricted types of hitokoto sentences
-   *
-   * Available types can be found on:
-   * https://developer.hitokoto.cn/sentence/#%E5%8F%A5%E5%AD%90%E7%B1%BB%E5%9E%8B-%E5%8F%82%E6%95%B0
-   */
-  defaultTypes?: string[];
-  /**
-   * For costumizing the hitokoto response format.
+   * For costumizing the response text format of this plugin.
    *
    * Available templates can be found on:
-   * https://github.com/AwesomeHamster/koishi-plugin-hitokoto/blob/master/src/template.ts
+   * https://github.com/AwesomeHamster/koishi-plugin-ffxiv-eorzea/blob/master/src/template.ts
    */
   template?: template.Node;
 }
@@ -29,10 +21,10 @@ export async function apply(ctx: Context, _config: EorzeaOptions = {}): Promise<
     .command("eorzea.time", template("eorzea.description"))
     .alias("et")
     .action(() => {
-        let et = ToEorzeaTime(new Date)
-        const data = {
-            eorzeaTime:et.getUTCHours() + ':' + et.getUTCMinutes() + ':' + et.getUTCSeconds()
-        };
-        return template("eorzea.format",data);
+      let et = ToEorzeaTime(new Date());
+      const data = {
+        eorzeaTime: et.getUTCHours() + ":" + et.getUTCMinutes() + ":" + et.getUTCSeconds(),
+      };
+      return template("eorzea.format", data);
     });
 }
