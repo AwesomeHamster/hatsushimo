7a6946a0a8f461e9234adff494d4cd8fad891136
迷子 (Maiko Tan) <maiko.tan.coding@gmail.com>
2022-03-20 09:55:32 +0000

    feat: add basic news command

---
+++ src/utils.ts
@@ -0,0 +1,29 @@
+import { Context } from "koishi";
+
+export type NewsCategory = "topics" | "notices" | "maintenance" | "updates" | "status" | "developers";
+export type NewsRagion = "na" | "jp" | "eu";
+
+export interface News {
+  id: string;
+  title: string;
+  url: string;
+  time: string;
+  image: string;
+  description: string;
+}
+
+export const getNews = async (
+  ctx: Context,
+  count: number,
+  category: NewsCategory = "topics",
+  ragion: NewsRagion = "jp",
+): Promise<News[]> => {
+  const url = `http://${ragion}.lodestonenews.com/news/${category}`;
+  const json = (await ctx.http.get(url)).data as News[];
+  if (json.length > count) return json.slice(0, count);
+  return json;
+};
+
+export const getWebpage = async (ctx: Context, url: string): Promise<string> => {
+  return (await ctx.http.get(url)).data as string;
+};
