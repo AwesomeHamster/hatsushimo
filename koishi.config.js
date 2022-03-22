59cf28329d6069c4b576df3c69ce31055cece6a5
迷子 (Maiko Tan) <maiko.tan.coding@gmail.com>
2022-03-22 02:08:23 +0000

    style: format via prettier

--- koishi.config.js
+++ koishi.config.js
@@ -1,150 +1,150 @@
 // eslint-disable-next-line @typescript-eslint/naming-convention
-const { Random } = require("koishi");
+const { Random } = require('koishi')
 
 try {
-  require("dotenv").config();
+  require('dotenv').config()
 } catch (e) {}
 
-const isDev = process.env.NODE_ENV === "dev";
+const isDev = process.env.NODE_ENV === 'dev'
 
 const devPlugins = [
-  "database-memory",
-  "console",
-  "sandbox",
-  "./plugins/eorzea",
-  "./plugins/hitokoto",
-  "./plugins/macrodict",
-  "./plugins/lodestone",
-  "pics",
-  "picsource-lolicon",
-];
+  'database-memory',
+  'console',
+  'sandbox',
+  './plugins/eorzea',
+  './plugins/hitokoto',
+  './plugins/macrodict',
+  './plugins/lodestone',
+  'pics',
+  'picsource-lolicon',
+]
 const prodPlugins = [
-  "adapter-discord",
-  "gocqhttp",
-  "adapter-onebot",
-  "database-mongo",
-  "admin",
-  "sudo",
-  "console",
-  "auth",
-  "status",
-  "chat",
-  "dataview",
-  "bind",
-  "callme",
-  "echo",
-  "recall",
-  "feedback",
-  "schedule",
-  "repeater",
-  "puppeteer",
-  "image-search",
-  "pics",
-  "picsource-lolicon",
-  "./plugins/eorzea",
-  "./plugins/hitokoto",
-  "./plugins/macrodict",
-];
+  'adapter-discord',
+  'gocqhttp',
+  'adapter-onebot',
+  'database-mongo',
+  'admin',
+  'sudo',
+  'console',
+  'auth',
+  'status',
+  'chat',
+  'dataview',
+  'bind',
+  'callme',
+  'echo',
+  'recall',
+  'feedback',
+  'schedule',
+  'repeater',
+  'puppeteer',
+  'image-search',
+  'pics',
+  'picsource-lolicon',
+  './plugins/eorzea',
+  './plugins/hitokoto',
+  './plugins/macrodict',
+]
 
 const plugins = {
-  "adapter-discord": {
-    selfId: process.env["HATSUSHIMO_DISCORD_SELF_ID"],
-    token: process.env["HATSUSHIMO_DISCORD_TOKEN"],
+  'adapter-discord': {
+    selfId: process.env['HATSUSHIMO_DISCORD_SELF_ID'],
+    token: process.env['HATSUSHIMO_DISCORD_TOKEN'],
   },
 
-  gocqhttp: {},
+  'gocqhttp': {},
 
-  "adapter-onebot": {
+  'adapter-onebot': {
     gocqhttp: true,
-    selfId: process.env["HATSUSHIMO_ONEBOT_SELF_ID"],
-    password: process.env["HATSUSHIMO_ONEBOT_PASSWORD"],
-    protocol: "ws",
-    token: process.env["HATSUSHIMO_ONEBOT_TOKEN"],
-    endpoint: "ws://localhost:6700/",
+    selfId: process.env['HATSUSHIMO_ONEBOT_SELF_ID'],
+    password: process.env['HATSUSHIMO_ONEBOT_PASSWORD'],
+    protocol: 'ws',
+    token: process.env['HATSUSHIMO_ONEBOT_TOKEN'],
+    endpoint: 'ws://localhost:6700/',
   },
 
-  "database-memory": {},
+  'database-memory': {},
 
-  "database-mongo": {
-    protocol: "mongodb+srv",
-    host: process.env["HATSUSHIMO_MONGO_HOST"] || "localhost",
-    username: process.env["HATSUSHIMO_MONGO_USERNAME"] || "root",
-    password: process.env["HATSUSHIMO_MONGO_PASSWORD"] || "",
-    database: process.env["HATSUSHIMO_MONGO_DATABASE"] || "koishiv4",
+  'database-mongo': {
+    protocol: 'mongodb+srv',
+    host: process.env['HATSUSHIMO_MONGO_HOST'] || 'localhost',
+    username: process.env['HATSUSHIMO_MONGO_USERNAME'] || 'root',
+    password: process.env['HATSUSHIMO_MONGO_PASSWORD'] || '',
+    database: process.env['HATSUSHIMO_MONGO_DATABASE'] || 'koishiv4',
   },
 
-  admin: {},
-  sudo: {},
-  console: {},
-  auth: {},
-  status: {},
-  chat: {},
-  dataview: {},
-  sandbox: {},
+  'admin': {},
+  'sudo': {},
+  'console': {},
+  'auth': {},
+  'status': {},
+  'chat': {},
+  'dataview': {},
+  'sandbox': {},
 
-  bind: {
-    generateToken: () => "hataushimo/" + Random.id(6, 10),
+  'bind': {
+    generateToken: () => 'hataushimo/' + Random.id(6, 10),
   },
-  callme: {},
-  echo: {},
-  recall: {},
-  feedback: {
-    operators: (process.env["HATSUSHIMO_FEEDBACK_OPERATORS"] || "").split(","),
+  'callme': {},
+  'echo': {},
+  'recall': {},
+  'feedback': {
+    operators: (process.env['HATSUSHIMO_FEEDBACK_OPERATORS'] || '').split(','),
   },
-  schedule: {},
-  repeater: {
+  'schedule': {},
+  'repeater': {
     onRepeat: {
       minTimes: 3,
       probability: 0.75,
     },
   },
 
-  puppeteer: {
-    browser: { args: ["--no-sandbox"] },
+  'puppeteer': {
+    browser: { args: ['--no-sandbox'] },
   },
 
-  "image-search": {
-    saucenaoApiKey: (process.env["HATSUSHIMO_SAUCENAO_API_KEY"] || "").split(","),
+  'image-search': {
+    saucenaoApiKey: (process.env['HATSUSHIMO_SAUCENAO_API_KEY'] || '').split(','),
   },
-  "./plugins/eorzea": {
+  './plugins/eorzea': {
     template: {
-      format: "现在艾欧泽亚时间是 {{eorzeaTime}}",
+      format: '现在艾欧泽亚时间是 {{eorzeaTime}}',
     },
   },
-  "./plugins/hitokoto": {
-    apiUrl: "https://international.v1.hitokoto.cn",
+  './plugins/hitokoto': {
+    apiUrl: 'https://international.v1.hitokoto.cn',
   },
-  "./plugins/macrodict": {
-    aliases: ["?", "宏学词典", "macro", "macros", "宏"],
-    defaultLanguage: "chs",
+  './plugins/macrodict': {
+    aliases: ['?', '宏学词典', 'macro', 'macros', '宏'],
+    defaultLanguage: 'chs',
     fetchOnStart: !!isDev,
   },
-  "./plugins/lodestone": {},
+  './plugins/lodestone': {},
 
-  pics: {
-    commandName: "setu",
+  'pics': {
+    commandName: 'setu',
   },
-  "picsource-lolicon": {
+  'picsource-lolicon': {
     isDefault: true,
   },
-};
+}
 
 const getPlugins = (isDev) => {
   if (isDev) {
-    return Object.fromEntries(Object.entries(plugins).filter(([name]) => devPlugins.includes(name)));
+    return Object.fromEntries(Object.entries(plugins).filter(([name]) => devPlugins.includes(name)))
   } else {
-    return Object.fromEntries(Object.entries(plugins).filter(([name]) => prodPlugins.includes(name)));
+    return Object.fromEntries(Object.entries(plugins).filter(([name]) => prodPlugins.includes(name)))
   }
-};
+}
 
 /**
  * @type {import("koishi").App.Config}
  */
 module.exports = {
-  host: "0.0.0.0",
+  host: '0.0.0.0',
   port: Number(process.env.PORT) || 8080,
-  nickname: ["hamster", "仓鼠"],
-  prefix: ["/", "."],
+  nickname: ['hamster', '仓鼠'],
+  prefix: ['/', '.'],
   delay: {
     character: 5,
     prompt: 60000,
@@ -152,7 +152,7 @@ module.exports = {
   autoAssign: true,
   autoAuthorize: isDev ? 2 : 1,
   request: {
-    proxyAgent: process.env["HTTP_PROXY"] || process.env["HTTPS_PROXY"] || undefined,
+    proxyAgent: process.env['HTTP_PROXY'] || process.env['HTTPS_PROXY'] || undefined,
   },
   plugins: getPlugins(isDev),
-};
+}
