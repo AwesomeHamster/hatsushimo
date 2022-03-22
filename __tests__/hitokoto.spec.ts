49b1759d255f7b4e6e2a68cbedb69167729e053c
迷子 (Maiko Tan) <maiko.tan.coding@gmail.com>
2022-03-22 02:07:47 +0000

    style: format via prettier

--- __tests__/hitokoto.spec.ts
+++ __tests__/hitokoto.spec.ts
@@ -1,39 +1,39 @@
-import { App } from "koishi";
-import mock from "@koishijs/plugin-mock";
-import memory from "@koishijs/plugin-database-memory";
+import { App } from 'koishi'
+import mock from '@koishijs/plugin-mock'
+import memory from '@koishijs/plugin-database-memory'
 
-import * as hitokoto from "../src";
+import * as hitokoto from '../src'
 
-describe("basic", () => {
-  const app = new App();
+describe('basic', () => {
+  const app = new App()
 
-  app.plugin(mock);
-  app.plugin(memory);
+  app.plugin(mock)
+  app.plugin(memory)
 
   app.plugin(hitokoto, {
     minLength: 10,
     maxLength: 10,
     template: {
       // to ease testing
-      format: "{{hitokoto}} | {{type}}",
+      format: '{{hitokoto}} | {{type}}',
     },
-  });
+  })
 
-  const client = app.mock.client("123");
+  const client = app.mock.client('123')
 
   before(async () => {
-    await app.mock.initUser("123", 4);
-  });
+    await app.mock.initUser('123', 4)
+  })
 
-  it("should reply hitokoto", async () => {
-    await client.shouldReply("hitokoto");
-  });
+  it('should reply hitokoto', async () => {
+    await client.shouldReply('hitokoto')
+  })
 
-  it("should reply a 10-length a-type hitokoto", async () => {
-    await client.shouldReply("hitokoto -t a", /^\w{10} | a$/);
-  });
+  it('should reply a 10-length a-type hitokoto', async () => {
+    await client.shouldReply('hitokoto -t a', /^\w{10} | a$/)
+  })
 
-  it("should reply a 20-length hitokoto", async () => {
-    await client.shouldReply("hitokoto -l 20 -L 20", /^\w{20} | .*$/);
-  });
-});
+  it('should reply a 20-length hitokoto', async () => {
+    await client.shouldReply('hitokoto -l 20 -L 20', /^\w{20} | .*$/)
+  })
+})
--- src/index.ts
+++ src/index.ts
@@ -1,139 +1,139 @@
-import { Context, Schema } from "koishi";
+import { Context, Schema } from 'koishi'
 
-import * as i18n from "./i18n";
+import * as i18n from './i18n'
 
 export interface HitokotoOptions {
   /**
    * @default "https://v1.hitokoto.cn"
    */
-  apiUrl?: string;
-  timeout?: number;
-  minLength?: number;
-  maxLength?: number;
+  apiUrl?: string
+  timeout?: number
+  minLength?: number
+  maxLength?: number
   /**
    * only reply restricted types of hitokoto sentences
    *
    * Available types can be found on:
    * https://developer.hitokoto.cn/sentence/#%E5%8F%A5%E5%AD%90%E7%B1%BB%E5%9E%8B-%E5%8F%82%E6%95%B0
    */
-  defaultTypes?: string[];
+  defaultTypes?: string[]
 }
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
 export const Config = Schema.object({
-  apiUrl: Schema.string().description("获取一言的API地址").default("https://v1.hitokoto.cn"),
-  timeout: Schema.number().description("请求API时的超时时间").default(3000),
-  minLength: Schema.number().description("一言的最小长度"),
-  maxLength: Schema.number().description("一言的最大长度"),
-  defaultTypes: Schema.array(Schema.string()).description("默认一言类别"),
-});
+  apiUrl: Schema.string().description('获取一言的API地址').default('https://v1.hitokoto.cn'),
+  timeout: Schema.number().description('请求API时的超时时间').default(3000),
+  minLength: Schema.number().description('一言的最小长度'),
+  maxLength: Schema.number().description('一言的最大长度'),
+  defaultTypes: Schema.array(Schema.string()).description('默认一言类别'),
+})
 
 export const types = {
-  a: "动画",
-  b: "漫画",
-  c: "游戏",
-  d: "文学",
-  e: "原创",
-  f: "来自网络",
-  g: "其他",
-  h: "影视",
-  i: "诗词",
-  j: "网易云",
-  k: "哲学",
-  l: "抖机灵",
-};
-
-export interface HitokotoRet {
-  id: number;
-  hitokoto: string;
-  type: string;
-  from: string;
-  from_who: string | null;
-  creator: string;
-  creator_uid: number;
-  reviewer: number;
-  uuid: string;
-  commit_from: string;
-  created_at: string;
-  length: number;
+  a: '动画',
+  b: '漫画',
+  c: '游戏',
+  d: '文学',
+  e: '原创',
+  f: '来自网络',
+  g: '其他',
+  h: '影视',
+  i: '诗词',
+  j: '网易云',
+  k: '哲学',
+  l: '抖机灵',
 }
 
-export const name = "hitokoto";
+export interface HitokotoRet {
+  id: number
+  hitokoto: string
+  type: string
+  from: string
+  from_who: string | null
+  creator: string
+  creator_uid: number
+  reviewer: number
+  uuid: string
+  commit_from: string
+  created_at: string
+  length: number
+}
+
+export const name = 'hitokoto'
 
 export async function apply(ctx: Context, _config: HitokotoOptions = {}): Promise<void> {
   const config = {
-    apiUrl: "https://v1.hitokoto.cn/",
+    apiUrl: 'https://v1.hitokoto.cn/',
     timeout: 3000,
     ..._config,
-  };
+  }
 
-  ctx.i18n.define("en", i18n.en);
-  ctx.i18n.define("zh", i18n.zh);
+  ctx.i18n.define('en', i18n.en)
+  ctx.i18n.define('zh', i18n.zh)
 
   ctx
-    .command("hitokoto")
-    .alias("一言")
-    .option("type", `-t <type:string>`)
-    .option("min-length", `-l <length:int>`)
-    .option("max-length", `-L <length:int>`)
+    .command('hitokoto')
+    .alias('一言')
+    .option('type', `-t <type:string>`)
+    .option('min-length', `-l <length:int>`)
+    .option('max-length', `-L <length:int>`)
     .before(async ({ options, session }) => {
-      if (typeof options?.type !== "undefined") {
+      if (typeof options?.type !== 'undefined') {
         if (!options.type) {
-          return session?.text(".invalid_type", [options.type]);
+          return session?.text('.invalid_type', [options.type])
         }
-        const types = options.type.split(",");
+        const types = options.type.split(',')
         if (types.length <= 0) {
-          return session?.text(".invalid_type", [options.type]);
+          return session?.text('.invalid_type', [options.type])
         } else {
           for (const type of types) {
             if (!type) {
-              return session?.text(".invalid_type", [options.type]);
+              return session?.text('.invalid_type', [options.type])
             }
           }
         }
       }
-      if (options?.["min-length"] && options?.["max-length"]) {
-        if (options["min-length"] > options["max-length"]) {
-          return session?.text(".min_length_gt_max_length");
+      if (options?.['min-length'] && options?.['max-length']) {
+        if (options['min-length'] > options['max-length']) {
+          return session?.text('.min_length_gt_max_length')
         }
       }
     })
     .action(async ({ options, session }) => {
-      const params = new URLSearchParams();
+      const params = new URLSearchParams()
       if (options?.type || config.defaultTypes) {
-        (options?.type?.split(",") || config.defaultTypes || []).forEach((type) => params.append("c", type));
+        ;(options?.type?.split(',') || config.defaultTypes || []).forEach((type) => params.append('c', type))
       }
-      if (options?.["min-length"] || config.minLength) {
-        params.append("min_length", options?.["min-length"] || config.minLength?.toString());
+      if (options?.['min-length'] || config.minLength) {
+        params.append('min_length', options?.['min-length'] || config.minLength?.toString())
       }
-      if (options?.["max-length"] || config.maxLength) {
-        params.append("max_length", options?.["max-length"] || config.maxLength?.toString());
+      if (options?.['max-length'] || config.maxLength) {
+        params.append('max_length', options?.['max-length'] || config.maxLength?.toString())
       }
 
       try {
         const resp = await ctx.http.get<HitokotoRet>(config.apiUrl, {
           timeout: config.timeout,
           params,
-        });
-        return session?.text(".format", {
+        })
+        return session?.text('.format', {
           ...resp,
           // the `from_who` field may be null.
-          from_who: resp.from_who ?? "",
-        });
+          from_who: resp.from_who ?? '',
+        })
       } catch (error) {
-        const err = error as Error;
+        const err = error as Error
         if (/ETIMEOUT/.test(err.message)) {
-          return session?.text(".timeout");
+          return session?.text('.timeout')
         }
-        return session?.text(".unknown_error", err);
+        return session?.text('.unknown_error', err)
       }
-    });
+    })
 
-  ctx.command("hitokoto.types").action(async ({ session }) => {
-    return session?.text(".list", [
+  ctx.command('hitokoto.types').action(async ({ session }) => {
+    return session?.text('.list', [
       Object.entries(types)
         .map(([type, desc]) => `${type} - ${desc}`)
-        .join("\n"),
-    ]);
-  });
+        .join('\n'),
+    ])
+  })
 }
