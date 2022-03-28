6e94c21d002b22a25ec30f62ccb8671100290e88
迷子 (Maiko Tan) <maiko.tan.coding@gmail.com>
2022-03-28 13:16:47 +0800

    refactor: full refactoring to make it koishier (#1)
    * refactor: koishify the whole plugin
    
    * fix: i18n not registered

--- src/command/eorzeaTime/eorzeaTimeTypes.ts
+++
@@ -1,6 +0,0 @@
-export { EorzeaTimeTypes }
-
-const EorzeaTimeTypes = {
-  KoishiEorzeaTimeInterface: Symbol.for('KoishiEorzeaTimeInterface'),
-  EorzeaTimeInterface: Symbol.for('EorzeaTimeInterface'),
-}
