351c1785ce40dfb6c2f2f9660a751b9399e67602
迷子 (Maiko Tan) <maiko.tan.coding@gmail.com>
2022-03-28 13:16:47 +0800

    refactor: full refactoring to make it koishier (#1)
    * refactor: koishify the whole plugin
    
    * fix: i18n not registered

--- src/command/eorzeaItem/eorzeaItemTypes.ts
+++
@@ -1,6 +0,0 @@
-export { EorzeaItemTypes }
-
-const EorzeaItemTypes = {
-  KoishiEorzeaItemInterface: Symbol.for('KoishiEorzeaItemInterface'),
-  EorzeaItemInterface: Symbol.for('EorzeaItemInterface'),
-}
--- src/command/eorzeaItem/service/eorzeaItemService.ts
+++
@@ -1,7 +0,0 @@
-import { injectable, inject } from 'inversify'
-import 'reflect-metadata'
-
-interface EorzeaItemInterface {}
-
-@injectable()
-export class EorzeaItemService implements EorzeaItemInterface {}
