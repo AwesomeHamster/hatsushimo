d073d3c04c5896f269d0d6c7ea393fc3eb0121db
迷子 (Maiko Tan) <maiko.tan.coding@gmail.com>
2022-04-08 01:56:01 +0000

    test: fix test for koishi v4

--- plugins/hitokoto/__tests__/hitokoto.spec.ts
+++ plugins/hitokoto/__tests__/hitokoto.spec.ts
@@ -2,38 +2,22 @@ import { App } from 'koishi'
 import mock from '@koishijs/plugin-mock'
 import memory from '@koishijs/plugin-database-memory'
 
-import * as hitokoto from '../src'
+import * as hitokoto from '..'
 
 describe('basic', () => {
   const app = new App()
 
   app.plugin(mock)
   app.plugin(memory)
-
   app.plugin(hitokoto, {
-    minLength: 10,
-    maxLength: 10,
-    template: {
-      // to ease testing
-      format: '{{hitokoto}} | {{type}}',
-    },
+    apiUrl: 'https://international.v1.hitokoto.cn/',
   })
 
   const client = app.mock.client('123')
 
-  before(async () => {
-    await app.mock.initUser('123', 4)
-  })
+  before(() => app.start())
 
   it('should reply hitokoto', async () => {
     await client.shouldReply('hitokoto')
-  })
-
-  it('should reply a 10-length a-type hitokoto', async () => {
-    await client.shouldReply('hitokoto -t a', /^\w{10} | a$/)
-  })
-
-  it('should reply a 20-length hitokoto', async () => {
-    await client.shouldReply('hitokoto -l 20 -L 20', /^\w{20} | .*$/)
   })
 })
