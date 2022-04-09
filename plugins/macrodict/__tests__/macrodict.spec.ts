827a0a3ca095e23abcc47fa65a644aa8b847e813
迷子 (Maiko Tan) <maiko.tan.coding@gmail.com>
2022-04-09 06:16:13 +0000

    test: fix macrodict test never ends

--- plugins/macrodict/__tests__/macrodict.spec.ts
+++ plugins/macrodict/__tests__/macrodict.spec.ts
@@ -1,25 +1,21 @@
 import { App } from 'koishi'
 import mock from '@koishijs/plugin-mock'
-import puppeteer from '@koishijs/plugin-puppeteer'
 import memory from '@koishijs/plugin-database-memory'
 
 import * as macrodict from '../src/index'
 
-describe('macrodict', async () => {
+describe('basic', async () => {
   const app = new App()
 
   app.plugin(mock)
   app.plugin(memory)
-  app.plugin(puppeteer)
   app.plugin(macrodict)
 
   await app.start()
 
   const client = app.mock.client('123')
 
-  before(async () => {
-    app.mock.initUser('123', 4)
-  })
+  before(async () => app.start())
 
   it('should render macro view', async () => {
     await client.shouldReply('macrodict hotbar')
