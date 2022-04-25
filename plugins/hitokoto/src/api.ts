e57669d9f0942c18115ecdc29432b026c517ebb3
迷子 (Maiko Tan) <maiko.tan.coding@gmail.com>
2022-04-25 08:15:06 +0000

    refactor(hitokoto): refactor the hitokoto module

--- plugins/hitokoto/src/api.ts
+++ plugins/hitokoto/src/api.ts
@@ -1,23 +1,51 @@
 import { Context, Service } from 'koishi'
-import { HitokotoOptions } from '.'
+import { Config } from '.'
+
+declare module 'koishi' {
+  namespace Context {
+    interface Services {
+      hitokoto: HitokotoApi
+    }
+  }
+}
+
+export interface HitokotoParams {
+  c?: string[]
+  min_length?: number
+  max_length?: number
+}
 
 export class HitokotoApi extends Service {
   private _apiUrl: string
 
-  constructor(ctx: Context, option: HitokotoOptions) {
+  constructor(ctx: Context, option: Config) {
     super(ctx, 'hitokoto', true)
     this._apiUrl = option.apiUrl ?? 'https://v1.hitokoto.cn/'
   }
 
-  async getHitokoto(params: Record<string, any> | URLSearchParams): Promise<HitokotoRet> {
+  async getHitokoto(params: HitokotoParams): Promise<HitokotoRet> {
     const resp = await this.ctx.http.get<HitokotoRet>(this._apiUrl, {
-      params,
+      params: this.buildSearchParams(params),
     })
     return {
       ...resp,
       // the `from_who` field may be null.
       from_who: resp.from_who ?? '',
     }
+  }
+
+  buildSearchParams(params: HitokotoParams): URLSearchParams {
+    const searchParams = new URLSearchParams()
+    if (params.c) {
+      params.c.forEach((type) => searchParams.append('c', type))
+    }
+    if (params.min_length) {
+      searchParams.append('min_length', params.min_length.toString())
+    }
+    if (params.max_length) {
+      searchParams.append('max_length', params.max_length.toString())
+    }
+    return searchParams
   }
 
   get types() {
---
+++ plugins/hitokoto/src/config.ts
@@ -0,0 +1,25 @@
+import { Schema } from 'koishi'
+
+export interface Config {
+  /**
+   * @default "https://v1.hitokoto.cn"
+   */
+  apiUrl?: string
+  minLength?: number
+  maxLength?: number
+  /**
+   * only reply restricted types of hitokoto sentences
+   *
+   * Available types can be found on:
+   * https://developer.hitokoto.cn/sentence/#%E5%8F%A5%E5%AD%90%E7%B1%BB%E5%9E%8B-%E5%8F%82%E6%95%B0
+   */
+  defaultTypes?: string[]
+}
+
+// eslint-disable-next-line @typescript-eslint/naming-convention
+export const Config = Schema.object({
+  apiUrl: Schema.string().description('获取一言的 API 地址').default('https://v1.hitokoto.cn'),
+  minLength: Schema.number().description('一言的最小长度'),
+  maxLength: Schema.number().description('一言的最大长度'),
+  defaultTypes: Schema.array(Schema.string()).description('默认一言类别'),
+})
