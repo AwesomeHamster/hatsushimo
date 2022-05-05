a6f1fca6df49451ccab67fa67e63cba594c6f041
迷子 (Maiko Tan) <maiko.tan.coding@gmail.com>
2022-05-05 09:38:31 +0000

    refactor: remove unused languages

--- plugins/eorzea/src/index.ts
+++ plugins/eorzea/src/index.ts
@@ -6,21 +6,15 @@ import * as i18n from './i18n'
 export const name = 'eorzea'
 
 export async function apply(ctx: Context): Promise<void> {
-  ctx.i18n.define('ar', i18n.ar)
   ctx.i18n.define('de', i18n.de)
   ctx.i18n.define('en', i18n.en)
-  ctx.i18n.define('eo', i18n.eo)
   ctx.i18n.define('es', i18n.es)
   ctx.i18n.define('fr', i18n.fr)
   ctx.i18n.define('ja', i18n.ja)
   ctx.i18n.define('ko', i18n.ko)
-  ctx.i18n.define('la', i18n.la)
-  ctx.i18n.define('lol', i18n.lol)
   ctx.i18n.define('ru', i18n.ru)
-  ctx.i18n.define('tlh', i18n.tlh)
-  ctx.i18n.define('uk', i18n.uk)
   ctx.i18n.define('zh', i18n.zh)
-  ctx.i18n.define('zht', i18n.zht)
+  ctx.i18n.define('zht', i18n.zhtw)
 
   ctx.plugin(time)
 }
