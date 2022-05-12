f5624445535acc15bde93cb2a320ab47ab66dec9
迷子 (Maiko Tan) <maiko.tan.coding@gmail.com>
2022-05-12 14:02:26 +0000

    fix: re-word some command descriptions

--- plugins/eorzea/src/index.ts
+++ plugins/eorzea/src/index.ts
@@ -14,7 +14,7 @@ export async function apply(ctx: Context): Promise<void> {
   ctx.i18n.define('ko', i18n.ko)
   ctx.i18n.define('ru', i18n.ru)
   ctx.i18n.define('zh', i18n.zh)
-  ctx.i18n.define('zht', i18n.zhtw)
+  ctx.i18n.define('zh-tw', i18n.zhtw)
 
   ctx.plugin(time)
 }
