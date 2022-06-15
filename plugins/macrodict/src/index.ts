69771bed72c4d0f85c037807fe6578cfa375862d
Maiko Tan <maiko.tan.coding@gmail.com>
2022-06-15 17:25:42 +0800

    refactor: redo i18n files

--- plugins/macrodict/src/index.ts
+++ plugins/macrodict/src/index.ts
@@ -2,7 +2,7 @@ import { Context } from 'koishi'
 
 import { Config } from './config'
 import { Updater } from './update'
-import * as i18n from './i18n'
+import i18n from './i18n'
 import { Search } from './search'
 
 declare module 'koishi' {
@@ -60,15 +60,8 @@ export async function apply(ctx: Context, _config: Config): Promise<void> {
     ..._config,
   }
 
-  ctx.i18n.define('de', i18n.de)
-  ctx.i18n.define('en', i18n.en)
-  ctx.i18n.define('es', i18n.es)
-  ctx.i18n.define('fr', i18n.fr)
-  ctx.i18n.define('ja', i18n.ja)
-  ctx.i18n.define('ko', i18n.ko)
-  ctx.i18n.define('ru', i18n.ru)
-  ctx.i18n.define('zh', i18n.zh)
-  ctx.i18n.define('zh-tw', i18n.zhtw)
+  // register i18n resources
+  Object.entries(i18n).forEach(([key, value]) => ctx.i18n.define(key, value))
 
   ctx.plugin(Search)
   ctx.plugin(Updater)
