f8fa6ebc501b204c137069d52c6e48b3c1289a6d
迷子 (Maiko Tan) <maiko.tan.coding@gmail.com>
2022-06-20 03:11:03 +0000

    refactor(macrodict): extract tools to utils

--- plugins/macrodict/__tests__/update.spec.ts
+++ plugins/macrodict/__tests__/update.spec.ts
@@ -1,13 +1,17 @@
 import { expect } from 'chai'
 import { App } from 'koishi'
 import mock from '@koishijs/plugin-mock'
+import * as memory from '@koishijs/plugin-database-memory'
 
 import { Updater } from '../src/update'
+import * as macrodict from '../src'
 
-describe('update', () => {
+describe('macrodict', () => {
   const app = new App()
 
   app.plugin(mock)
+  app.plugin(memory)
+  app.plugin(macrodict)
 
   const client = app.mock.client('123')
   const ctx = client.mock.ctx
@@ -17,26 +21,35 @@ describe('update', () => {
     app.start()
   })
 
-  it('fetch international macros', async () => {
-    const data = await updater.fetchIntl()
-    expect(data.length).greaterThan(0)
-  })
-    .timeout(0)
-    .retries(3)
-
-  it('fetch chinese macros', async () => {
-    const data = await updater.fetchCn()
-    expect(data.length).greaterThan(0)
-  })
-    .timeout(0)
-    .retries(3)
-
-  it('fetch korean macros', async () => {
-    const data = await updater.fetchKo()
-    expect(data.length).greaterThan(0)
-  })
-    .timeout(0)
-    .retries(3)
-
   after(() => app.stop())
+
+  describe('update', () => {
+    it('fetch international macros', async () => {
+      const data = await updater.fetchIntl()
+      expect(data.length).greaterThan(0)
+    })
+      .timeout(0)
+      .retries(3)
+
+    it('fetch chinese macros', async () => {
+      const data = await updater.fetchCn()
+      expect(data.length).greaterThan(0)
+    })
+      .timeout(0)
+      .retries(3)
+
+    it('fetch korean macros', async () => {
+      const data = await updater.fetchKo()
+      expect(data.length).greaterThan(0)
+    })
+      .timeout(0)
+      .retries(3)
+
+    it('update database', async () => {
+      const count = await updater.update()
+      expect(count).greaterThan(0)
+    })
+      .timeout(0)
+      .retries(3)
+  })
 })
