f72a30c7cf98e79b76de245e251b5332f1cc3ab2
迷子 (Maiko Tan) <maiko.tan.coding@gmail.com>
2022-06-30 06:05:42 +0000

    feat(lodestone-news): add more categories

--- packages/lodestone-news/src/config.ts
+++ packages/lodestone-news/src/config.ts
@@ -12,28 +12,55 @@ const locales = {
   },
 }
 
+const baseRule: Pick<Rule, 'rootNode' | 'page' | 'items'> = {
+  rootNode: ($) =>
+    $('#news')
+      .children('div.ldst__bg')
+      .children('div.ldst__contents')
+      .children('div.ldst__main')
+      .children('div.news__content'),
+  page(el, $, context) {
+    const pager = el
+      .find('ul.btn__pager > li.btn__pager__current')
+      .first()
+      .text()
+    const m = locales.pager[context.locale].exec(pager)
+    return {
+      current: parseInt(m?.[1] ?? '0', 10),
+      total: parseInt(m?.[2] ?? '0', 10),
+    }
+  },
+  items(el, $, context) {
+    // There might be a "Special Notice" section, but we don't need it.
+    // So we have to find the news item without the "parts__space--add" class.
+    const items = el.find('ul:not(.parts__space--add) > li.news__list')
+    return items
+      .map((i, el) => {
+        const $a = $(el).find('a')
+
+        const title = $a.find('p.news__list--title').text()
+        const url = $a.attr('href')!
+
+        const $timeScript = $a.find('time.news__list--time > script').text()
+        const time = $timeScript.match(/ldst_strftime\((\d+), 'YMD'\);/)?.[1]!
+        const epoch = parseInt(time, 10)
+        return {
+          title,
+          epoch,
+          date: new Date(epoch * 1000),
+          url: fixUrl(url, context.referer),
+        }
+      })
+      .toArray()
+  },
+}
+
 const config: Config = {
   rules: {
     topics: {
+      ...baseRule,
       url: ({ locale, page }) =>
         `https://${locale}.finalfantasyxiv.com/lodestone/topics/?page=${page}`,
-      rootNode: ($) =>
-        $('#news')
-          .children('div.ldst__bg')
-          .children('div.ldst__contents')
-          .children('div.ldst__main')
-          .children('div.news__content'),
-      page(el, $, context) {
-        const pager = el
-          .find('ul.btn__pager > li.btn__pager__current')
-          .first()
-          .text()
-        const m = locales.pager[context.locale].exec(pager)
-        return {
-          current: parseInt(m?.[1] ?? '0', 10),
-          total: parseInt(m?.[2] ?? '0', 10),
-        }
-      },
       items(el, $, context) {
         const items = el.find('li.news__list--topics')
         return items
@@ -61,6 +88,26 @@ const config: Config = {
           })
           .toArray()
       },
+    },
+    notices: {
+      ...baseRule,
+      url: ({ locale, page }) =>
+        `https://${locale}.finalfantasyxiv.com/lodestone/news/category/1?page=${page}`,
+    },
+    maintenance: {
+      ...baseRule,
+      url: ({ locale, page }) =>
+        `https://${locale}.finalfantasyxiv.com/lodestone/news/category/2?page=${page}`,
+    },
+    updates: {
+      ...baseRule,
+      url: ({ locale, page }) =>
+        `https://${locale}.finalfantasyxiv.com/lodestone/news/category/3?page=${page}`,
+    },
+    status: {
+      ...baseRule,
+      url: ({ locale, page }) =>
+        `https://${locale}.finalfantasyxiv.com/lodestone/news/category/4?page=${page}`,
     },
   },
 }
