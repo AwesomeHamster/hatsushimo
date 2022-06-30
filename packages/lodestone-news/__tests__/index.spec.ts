e2dca79da975cf8b2058761c347cd156e71e549b
迷子 (Maiko Tan) <maiko.tan.coding@gmail.com>
2022-06-30 06:05:42 +0000

    feat(lodestone-news): add more categories

--- packages/lodestone-news/__tests__/index.spec.ts
+++ packages/lodestone-news/__tests__/index.spec.ts
@@ -3,18 +3,30 @@ import getNews, { locales } from '../src'
 
 describe('lodestone-news', () => {
   locales.forEach((locale) => {
-    it(`should get ${locale} news`, async () => {
-      const resp = await getNews(locale)
-      const { news, page } = resp
-      expect(news).is.an('array')
-      expect(news).has.length.greaterThan(0)
-      expect(news[0]).has.property('title').which.is.a('string')
-      expect(news[0]).has.property('epoch').which.is.a('number')
-      expect(news[0]).has.property('url').which.is.a('string')
-      expect(news[0]).has.property('date').which.is.a('date')
+    describe(`Locale: ${locale}`, () => {
+      ;['topics', 'notices', 'maintenance', 'updates', 'status'].forEach(
+        (cat) => {
+          it(`should fetch ${cat}`, async () => {
+            const resp = await getNews(locale)
+            const { news, page } = resp
+            expect(news).is.an('array')
+            expect(news).has.length.greaterThan(0)
+            expect(news[0]).has.property('title').which.is.a('string')
+            expect(news[0]).has.property('epoch').which.is.a('number')
+            expect(news[0]).has.property('url').which.is.a('string')
+            expect(news[0]).has.property('date').which.is.a('date')
 
-      expect(page).has.property('current').which.is.a('number').greaterThan(0)
-      expect(page).has.property('total').which.is.a('number').greaterThan(0)
-    }).timeout(0)
+            expect(page)
+              .has.property('current')
+              .which.is.a('number')
+              .greaterThan(0)
+            expect(page)
+              .has.property('total')
+              .which.is.a('number')
+              .greaterThan(0)
+          }).timeout(0)
+        },
+      )
+    })
   })
 })
